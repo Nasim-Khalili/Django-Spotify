8aa82d88d4bcbb78f4a19c7b26eae0070d3a6032
Nasim-Khalili <khalilinassim85@gmail.com>
2025-03-25 13:10:04 +0330

    add func for play music from youtube ...

--- main/models.py
+++ main/models.py
@@ -12,3 +12,12 @@ class Song(models.Model):
     
     def __str__(self):
         return self.title
+    
+    
+    def youtube_url_modify(self):
+        if 'youtu.be' in self.audio_url:
+            video_id = self.audio_url.split("/")[-1].split("?si=")[0]
+        elif "youtube.com" in self.audio_url:
+            video_id = self.audio_url.split("v=")[-1].split("&")[0]
+
+        return f"https://www.youtube.com/embed/{video_id}?autoplay=1&controls=0&showinfo=0&autohide=1"
