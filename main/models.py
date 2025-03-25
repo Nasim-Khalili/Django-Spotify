d74d8ef8b81bc100cae96196f56f5dbd779276f3
Nasim-Khalili <khalilinassim85@gmail.com>
2025-03-25 11:56:04 +0330

    Songs model added

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# Songs Model
+
+class Song(models.Model):
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=200)
+    gerne = models.CharField(max_length=200)
+    audio_url = models.CharField(max_length=300)
+    duration = models.CharField(max_length=20)
+    cover_image = models.ImageField(upload_to='cover_image/',blank=True,null=True)
+    
+    def __str__(self):
+        return self.title
